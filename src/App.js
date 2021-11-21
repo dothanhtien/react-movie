824bc9b77edfeb1dccc48d0efe16bbe6e0070980
Tien Do Thanh <dothanhtien95@gmail.com>
2021-11-21 21:40:56 +0700

    protect routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ACCESS_TOKEN } from "./utils/constants/appConfig";
+import { IgnoreAuth, RequireAuth } from "./HOCs/routes";
+import { fetchMe } from "./store/actions/auth";
 
 // views
 import SignIn from "./views/Auth/SignIn";
@@ -9,13 +13,50 @@ import Home from "./views/Home";
 import Admin from "./views/Admin";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      dispatch(fetchMe);
+    }
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/signin" exact element={<SignIn />} />
-        <Route path="/signup" exact element={<SignUp />} />
-        <Route path="/admin" exact element={<Admin />} />
+        <Route
+          path="/"
+          element={
+            <IgnoreAuth redirectPath="/admin">
+              <Home />
+            </IgnoreAuth>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <IgnoreAuth redirectPath="/admin">
+              <SignIn />
+            </IgnoreAuth>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <IgnoreAuth redirectPath="/admin">
+              <SignUp />
+            </IgnoreAuth>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <RequireAuth redirectPath="/signin">
+              <Admin />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,6 +1,7 @@
 import { createAction } from ".";
 import { actionType } from "./type";
 import { authService } from "../../services";
+import { ACCESS_TOKEN } from "../../utils/constants/appConfig";
 
 export const signIn = (data, callback) => {
   return async (dispatch) => {
@@ -9,6 +10,7 @@ export const signIn = (data, callback) => {
     try {
       const res = await authService.signIn(data);
 
+      localStorage.setItem(ACCESS_TOKEN, res.data.content.accessToken);
       dispatch(createAction(actionType.SIGNIN_SUCCESS, res.data.content));
 
       if (callback) callback();
@@ -33,4 +35,19 @@ export const signUp = (data, callback) => {
       dispatch(createAction(actionType.SET_AUTH_ERROR, err.response.data));
     }
   };
+};
+
+export const fetchMe = async (dispatch) => {
+  try {
+    const res = await authService.fetchMe();
+
+    dispatch(createAction(actionType.SIGNIN_SUCCESS, res.data.content));
+  } catch (err) {
+    console.log(err);
+
+    if (err.response.status === 401) {
+      localStorage.removeItem(ACCESS_TOKEN);
+      window.location.reload();
+    }
+  }
 };
